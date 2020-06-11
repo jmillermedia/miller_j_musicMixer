f4deee29ca41081c59612ea998bd1f255a7272a5
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-06-11 09:00:15 -0400

    Check devNotes on Github
    Just adding what I have do far and changed the devNotes from txt to markdown.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 // JAVASCRIPT START
+
+(() => {
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+	loopButtons = document.querySelectorAll('.loopButton'),
+	dropZones = document.querySelectorAll('.dropZone');
+
+	function allowDrag(event) {
+		console.log('Started dragging the image: ', event.target.id);
+
+		event.dataTransfer.setData('draggedImage', this.id);
+	}
+
+	function allowDragOver(event) {
+		console.log('Dragged something over me!');
+		event.preventDefault();
+	}
+
+	function allowDrop(event) {
+		console.log('dropped on me');
+		let droppedImage = event.dataTransfer.getData('draggedImage');
+		
+		event.target.appendChild(document.querySelector(`#${droppedImage}`));
+	}
+
+	puzzleButtons.forEach(piece => piece.addEventListener('dragStart', allowDrag));
+	// debugger;
+
+	dropZones.forEach(zone => {
+		zone.addEventListener('dragover', allowDragOver);
+		zone.addEventListener('drop', allowDrop);
+	})
+
+	console.log(puzzleButtons);
+})();
