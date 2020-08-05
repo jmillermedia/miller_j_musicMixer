b1cd406492d6e66a3a39b318ad0c545ca86d8dbc
Jon <59712501+jmillermedia@users.noreply.github.com>
2020-08-04 20:51:38 -0400

    random commit

--- js/main.js
+++ js/main.js
@@ -4,28 +4,24 @@
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img'), //adds all img tags into a node list
 	loopButtons = document.querySelectorAll('.loopButton'),
 	dropZones = document.querySelectorAll('.dropZone'),
-	audioLoops = document.querySelectorAll('.audioLoops');
+	audioLoops = document.querySelectorAll('.audioLoops'),
+	buttonHole = document.querySelectorAll('.buttonHole');
 
 	function allowDrag(event) {
-		console.log('Started dragging the image: ', event.target.id);
-
 		event.dataTransfer.setData('draggedImage', this.id);
 	}
 
 	function allowDragOver(event) {
-		console.log('Dragged something over me!');
 		event.preventDefault();
 	}
 
 	function allowDrop(event) {
-		console.log('dropped on me');
 		let droppedImage = event.dataTransfer.getData('draggedImage'),
     	audioLoop = document.querySelector(`audio[data-key="${droppedImage}"]`);
 		
 		if (this.children.length > 0) { // if the div already has a child, stop the code
 			return;
 		}
-		console.log(audioLoop);
 		this.appendChild(document.querySelector(`#${droppedImage}`));
 		this.querySelector(`#${droppedImage}`).classList.toggle('noFilter'); // removes the greyscale filter by toggling a CSS rule
 
@@ -38,10 +34,26 @@
 	
 	}
 
-	// click on a button to remove it and stop the audio loop
-	function removeButton(e) {
-		let removeImage = event.dataTransfer.getData
-		this.removeChild;
+	function allowStopage() {
+		event.preventDefault();
+	}
+
+	function allowStopDrop() {
+		let droppedStop = event.dataTransfer.getData('draggedImage'),
+    	audioLoop = document.querySelector(`audio[data-key="${droppedStop}"]`);
+		
+		if (this.children.length > 0) { // if the div already has a child, stop the code
+			return;
+		}
+		this.appendChild(document.querySelector(`#${droppedStop}`));
+		this.querySelector(`#${droppedStop}`).classList.toggle('noFilter'); // removes the greyscale filter by toggling a CSS rule
+
+		if (!audioLoop) { // ! tests for a negative result
+			return;
+		} 
+	
+		audioLoop.currentTime = 0;
+		audioLoop.pause();
 	}
 
 	loopButtons.forEach(button => button.addEventListener('dragstart', allowDrag));
@@ -50,7 +62,11 @@
 	dropZones.forEach(zone => {
 		zone.addEventListener('dragover', allowDragOver); // listen for dragover and run allowDragOver function when it occurs
 		zone.addEventListener('drop', allowDrop); // listen for drop event and run allowDrop function when it occurs
-		zone.addEventListener('click', removeButton);
+	})
+
+	buttonHole.forEach(hole => {
+		hole.addEventListener('dragover', allowStopage);
+		hole.addEventListener('drop', allowStopDrop);
 	})
 
 	loopButtons.forEach(zone => {
@@ -58,5 +74,4 @@
 		zone.addEventListener('drop', allowDrop);
 	})
 
-	console.log(audioLoops);
 })();
